cf9c6e37222db7b7f815ac07ab944701d54e5691
mrityunjayshukla411 <69010998+mrityunjayshukla411@users.noreply.github.com>
2025-01-14 14:53:43 +0000

    Deploying to gh-pages from @ mrityunjayshukla411/mrityunjayshukla411.github.io@562d701f338a50c1976a9a9ccc1f2cd376ec383d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-joined-the-sharc-research-group",
+          title: 'Joined the SHArC Research Group.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -492,14 +484,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%72%69%74%79%75%6E%6A%61%79%73%68%75%6B%6C%61%34%31%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/mrityunjayshukla411", "_blank");
         },
       },{
         id: 'social-rss',
@@ -507,13 +499,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
